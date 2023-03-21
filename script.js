6dc0f40154c0a266273e21c768ee0bf7165d3809
Raajulogu <111479920+Raajulogu@users.noreply.github.com>
2023-03-21 15:11:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var main=document.createElement("div");
+main.setAttribute("class", "main");
+document.body.appendChild(main);
+var count=10;
+var content="Happy New Year";
+setTimeout(()=>{main.innerHTML=count
+count--;
+setTimeout(()=>{main.innerHTML=count
+    count--;
+    setTimeout(()=>{main.innerHTML=count
+        count--;
+        setTimeout(()=>{main.innerHTML=count
+            count--;
+            setTimeout(()=>{main.innerHTML=count
+                count--;
+                setTimeout(()=>{main.innerHTML=count
+                    count--;
+                    setTimeout(()=>{main.innerHTML=count
+                        count--;
+                        setTimeout(()=>{main.innerHTML=count
+                            count--;
+                            setTimeout(()=>{main.innerHTML=count
+                                count--;
+                                setTimeout(()=>{main.innerHTML=count
+                                    count--;
+                                    setTimeout(()=>{main.innerHTML=content;
+                                        
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
